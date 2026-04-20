e86dcb2756cf6270353e9f2fb90d75cbdf8d01ad
Wafa458 <A00336458@student.tus.ie>
2026-04-20 17:06:27 +0100

    good pipline

--- order/src/main/java/com/tus/order/service/CustomerService.java
+++ order/src/main/java/com/tus/order/service/CustomerService.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 @Service
 public class CustomerService {
 private final CustomerRepository repo;
-public String password = "wrong password";
+
 public CustomerService(CustomerRepository repo) {
         this.repo = repo;
         
